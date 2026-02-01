4e975850f020198c34795de664f6dafda0f80362
Shrey Shah <142074262+shreyshah2003@users.noreply.github.com>
2026-02-01 21:51:10 +0530

    Add files via upload

---
+++ DSA 29.01.26/Square.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    
+    int a,b;
+    cin >> a;
+    for (int i = 1; i <= a; i++)
+    {
+        for(int j = a; j >=1 ; j--)
+        {
+            cout << "*";
+        }
+        cout << endl;
+        
+    }
+ 
+}
